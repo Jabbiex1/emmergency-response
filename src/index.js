e898b30e659074245c9f396165ffdcdc7ab8044a
Abdulai Nuru Jabbie <jabbieabdulai582@gmail.com>
2026-03-04 20:24:04 +0000

    Week 6 - mobile app complete

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const cors = require('cors');
 const db = require('./db');
 require('dotenv').config();
 
@@ -16,6 +17,7 @@ const io = new Server(server, {
   }
 });
 
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
@@ -43,28 +45,21 @@ app.get('/db-test', async (req, res) => {
 io.on('connection', (socket) => {
   console.log('A user connected:', socket.id);
 
-  // User joins their own incident room
   socket.on('join_incident', (incidentId) => {
     socket.join(incidentId);
     console.log(`User joined incident room: ${incidentId}`);
   });
 
-  // Responder updates their location
   socket.on('update_location', (data) => {
     const { incidentId, latitude, longitude } = data;
-    // Broadcast location to everyone in the incident room
     io.to(incidentId).emit('responder_location', { latitude, longitude });
-    console.log(`Location update for incident ${incidentId}:`, latitude, longitude);
   });
 
-  // Responder updates incident status
   socket.on('update_status', (data) => {
     const { incidentId, status } = data;
     io.to(incidentId).emit('incident_status', { status });
-    console.log(`Status update for incident ${incidentId}: ${status}`);
   });
 
-  // User and responder messaging
   socket.on('send_message', (data) => {
     const { incidentId, message, sender } = data;
     io.to(incidentId).emit('new_message', { message, sender, time: new Date() });
